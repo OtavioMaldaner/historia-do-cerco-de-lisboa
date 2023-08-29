c421858ce9e673cf75de79df757b09b1f11b61af
OtavioMaldaner <otaviomaldaner571@gmail.com>
2023-08-29 20:48:09 -0300

    upload

--- cerco-de-lisboa/src/app/components/Footer.tsx
+++ cerco-de-lisboa/src/app/components/Footer.tsx
@@ -1,17 +1,21 @@
+import { FaGithubSquare, FaInstagramSquare } from 'react-icons/fa'
+import { SiGmail } from 'react-icons/si'
 import styles from '../../../styles/footer.module.scss';
 const Footer = () => {
     // https://youtu.be/q_3tBjkcD80?si=YaApi2RlakH99Sc9
-  return (
-    <div className={styles.footer}>
-        <div className={styles.footerUtil}>
-            <ul>
-                <div>
-
-                </div>
-            </ul>
+    return (
+        <div className={styles.footer}>
+            <div className={styles.footerUtil}>
+                <ul>
+                    <div className={styles.icons}>
+                        <FaInstagramSquare />
+                        <FaGithubSquare />
+                        <SiGmail />
+                    </div>
+                </ul>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
