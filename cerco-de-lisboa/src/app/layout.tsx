510a61916713d8160ca8fd70eef0d4c3248fba82
OtavioMaldaner <otaviomaldaner571@gmail.com>
2023-09-03 17:19:22 -0300

    homepage fix

--- cerco-de-lisboa/src/app/layout.tsx
+++ cerco-de-lisboa/src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next';
-import { Nunito, Roboto_Flex as Roboto } from 'next/font/google';
+import { Nunito, Roboto_Flex as Roboto, Sacramento } from 'next/font/google';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import './globals.css';
 
 const roboto = Roboto({ subsets: ['latin'] })
 const nunito = Nunito({ subsets: ['latin'] })
+const sacramento = Sacramento({ subsets: ['latin'] , weight: '400'})
 export const metadata: Metadata = {
     title: 'História do Cerco de Lisboa',
     description: 'Site dedicado a experiência da leitura do livro História do Cerco de Lisboa, de José Saramago.',
@@ -17,7 +18,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="pt-br" className={`${roboto.className} ${nunito.className}`}>
+        <html lang="pt-br" className={`${roboto.className} ${nunito.className} ${sacramento.className}`}>
             <Header />
             <body>{children}</body>
             <Footer />
--- cerco-de-lisboa/src/app/page.tsx
+++ cerco-de-lisboa/src/app/page.tsx
@@ -1,11 +1,21 @@
 import { Analytics } from '@vercel/analytics/react';
+import Image from 'next/image';
 import styles from '../../styles/page.module.scss';
+import lisbon from './assets/images/wide-shot-boats-body-water-near-houses-buildings-porto-portugal.jpg';
 import Card from './components/Card';
 import { cards } from './pageContent';
 
 export default function Home() {
     return (
         <main className={styles.main}>
+            <div className={styles.homeImage}>
+                <Image src={lisbon} alt='Lisbon' quality={100} height={1000} placeholder='blur' />
+                <div>
+                    <h1 className={styles.title}>História do Cerco de Lisboa</h1>
+                    <h2 className={styles.subtitle}>de José Saramago</h2>
+                </div>
+                <span>Site dedicado à leitura da obra do autor português</span>
+            </div>
             <div className={styles.mainUtil}>
                 <div className={styles.content}>
                     {
