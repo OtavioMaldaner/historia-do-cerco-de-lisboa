af30b154f5411136fbe40ff8ce199a50f0427067
OtavioMaldaner <otaviomaldaner571@gmail.com>
2023-09-03 17:43:29 -0300

    navbar

--- cerco-de-lisboa/src/app/components/Header.tsx
+++ cerco-de-lisboa/src/app/components/Header.tsx
@@ -1,42 +1,66 @@
-'use client'
-import Image from 'next/image';
-import styles from '../../../styles/header.module.scss';
-import bandeira from '../assets/images/bandeira-portugal-2.jpg'
-import Link from 'next/link';
-import { FiMenu } from 'react-icons/fi';
-import { useState } from 'react';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import styles from "../../../styles/header.module.scss";
+import bandeira from "../assets/images/bandeira-portugal-2.jpg";
 const Header = () => {
     const [open, setOpen] = useState<boolean>(false);
     return (
-
         // Link: https://youtu.be/k7LrSw_pl2g?si=Qeq_TrZnbJ3KZ26D
-        <header className={styles.mainHeader}>
-
-            <div className={styles.mainHeaderUtil}>
-                <div className={styles.bookInfos}>
-                    <div className={styles.icon}>
-                        <Image src={bandeira} alt='Bandeira Portugal' height={30} width={30} />
+        <>
+            <header className={styles.mainHeader}>
+                <div className={styles.mainHeaderUtil}>
+                    <div className={styles.bookInfos}>
+                        <div className={styles.icon}>
+                            <Image
+                                src={bandeira}
+                                alt="Bandeira Portugal"
+                                height={30}
+                                width={30}
+                            />
+                        </div>
+                        <div className={styles.details}>
+                            <strong>
+                                <span>História do Cerco de Lisboa</span>
+                            </strong>
+                            <span>José Saramago</span>
+                        </div>
                     </div>
-                    <div className={styles.details}>
-                        <strong><span>História do Cerco de Lisboa</span></strong>
-                        <span>José Saramago</span>
-                    </div>
+                    <FiMenu
+                        clasName={styles.menuIcon}
+                        onClick={() => {
+                            setOpen(!open);
+                        }}
+                    />
                 </div>
-                <FiMenu clasName={styles.menuIcon} onClick={() => {
-                    setOpen(!open)
-                }} />
-                {/* <nav className={styles.headerNav} style={open ? {display: 'block'} : {display: 'none'}}>
+            </header>
+            {open && (
+                <nav className={styles.headerNav}>
                     <ul>
-                        <li><Link href='/'>Home</Link></li>
-                        <li><Link href='/author'>Autor</Link></li>
-                        <li><Link href='/history'>História</Link></li>
-                        <li><Link href='/history/characters'>Personagens</Link></li>
-                        <li><Link href='/opinions'>Opiniões</Link></li>
-                        <li><Link href='/references'>Referências</Link></li>
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/author">Autor</Link>
+                        </li>
+                        <li>
+                            <Link href="/history">História</Link>
+                        </li>
+                        <li>
+                            <Link href="/history/characters">Personagens</Link>
+                        </li>
+                        <li>
+                            <Link href="/opinions">Opiniões</Link>
+                        </li>
+                        <li>
+                            <Link href="/references">Referências</Link>
+                        </li>
                     </ul>
-                </nav> */}
-            </div>
-        </header>
-    )
-}
+                </nav>
+            )}
+        </>
+    );
+};
 export default Header;
